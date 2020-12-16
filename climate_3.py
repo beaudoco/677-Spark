5cd8d9917825c42c34fd4467b95bb5a9f951803c
beaudoco <39250947+beaudoco@users.noreply.github.com>
2020-12-16 03:40:02 -0500

    Update climate_3.py
    Adding comments

--- climate_3.py
+++ climate_3.py
@@ -17,15 +17,19 @@ def mapper(word):
 
 
 if __name__ == "__main__":
-
+#   YEAR LIST
     year_list = ['1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995',
                  '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012']
 #     year_list = ['1980']
+#   SPARK PROGRAM
     sc = SparkContext("local[*]", appName="PySparkClimate1981")
+#     RUN FOR TIMING
     for x in range(0, 5):
         start_time = time.time()
+#       RUN FOR EACH YEAR
         for year in year_list:
-            # f = open(year + ".txt", "w")
+            f = open(year + ".txt", "w")
+#           GO THRU EACH FILE
             for fileName in glob.glob("/home/DATA/NOAA_weather/" + year + "/*.gz"):
                 lines = sc.textFile(fileName, 1)
                 counts = lines.flatMap(lambda x: x.splitlines()) \
@@ -33,10 +37,9 @@ if __name__ == "__main__":
                     .filter(lambda x: float(x) < float("9999")) \
                     .mean()
                 output = counts
-                # output = counts.collect()
-                # print(output)
-                # f.write(str(output) + ",")
-            # f.close()
+#               ADD MEAN TO FILE
+                f.write(str(output) + ",")
+            f.close()
         end_time = time.time()
         print("TIME OF PROGRAM: ", end_time - start_time)
     sc.stop()
